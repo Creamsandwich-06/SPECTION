138f818a1ec47918db7e89d98c0121ac2ed5d823
Gianfranco <68628178+Creamsandwich-06@users.noreply.github.com>
2022-05-10 18:29:32 +0800

    Added existing files including Proposal

---
+++ Thesis Project/User Interface/myscript.js
@@ -0,0 +1,5 @@
+const btn=document.querySelector(".drops");
+const container2= document.querySelector(".dropdown");
+	btn.addEventListener("click",()=> {container2.classList.toggle("change")});
+
+	// Data Picker Initialization
