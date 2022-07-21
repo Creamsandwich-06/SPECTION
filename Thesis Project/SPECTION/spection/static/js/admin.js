575720d46ae1c2f4a9c11829cfcc9e2dc8feb657
Oatmeals-hub <68628178+Creamsandwich-06@users.noreply.github.com>
2022-07-21 22:56:39 +0800

    Updated

--- Thesis Project/SPECTION/spection/static/js/admin.js
+++ Thesis Project/SPECTION/spection/static/js/admin.js
@@ -1,6 +1,1 @@
-var jsonstr = '[{"id": 17, "name": "New Year", "badge": "test", "date": "2022-01-01", "type": "Holiday", "everyYear": true, "description": "Happy New Year to you!", "color": "#ffa808"}]'
-[1, 2, 3, 4]
-[1, 2, 3, 4]
-[1, 2, 3, 4]
-[1, 2, 3, 4]
-[1, 2, 3, 4]
+var jsonstr = '[{"id": 1, "name": "New Year", "badge": "Holiday", "date": "2022-01-01", "type": "Holiday", "everyYear": true, "description": "Happy New Year Everyone", "color": "#ffa808"}]'
